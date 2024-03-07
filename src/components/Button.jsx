d07a695ae80cebfb2cb74fdde59bd812a9d1eab4
Robin Tapiru <robintapiru0894@gmail.com>
2024-03-07 17:37:03 +0400

    Subslll Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,14 +4,15 @@ const Button = ({
   backgroundColor,
   borderColor,
   textColor,
+  fullWidth,
 }) => {
   return (
     <button
-      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full ${
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none ${
         backgroundColor
-          ? `${backgroundColor}} ${textColor} ${borderColor}`
+          ? `${backgroundColor} ${textColor} ${borderColor}`
           : "bg-coral-red  text-white border-coral-red"
-      }`}
+      }  rounded-full ${fullWidth && "w-full"}`}
     >
       {label}
       {iconURL && (
