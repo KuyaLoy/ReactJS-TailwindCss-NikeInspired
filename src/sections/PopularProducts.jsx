6676b3065d16402516275bd81b4f31730667c02d
KuyaLoy <robintapiru0894@gmail.com>
2024-06-15 13:55:45 +0400

    fix error

--- src/sections/PopularProducts.jsx
+++
@@ -1,9 +0,0 @@
-import ProductPopularCard from "../components/ProductPopularCard";
-import { products } from "../constants";
-
-const PopularProducts = () => {
-  return (
-    
-  );
-};
-export default PopularProducts;
